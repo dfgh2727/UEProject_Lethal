b45c8d83fb104f94adeb929d6b78c575b9cca5a0
dfgh2727 <dfgh2727@naver.com>
2025-07-15 01:06:34 +0900

    이수환: 각 파트 폴더 추가, GameInstance 추가

---
+++ Project_Lethal/Source/Project_Lethal/Global/LCGameInstance.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+//#include "Global/LCGameInstance.h"
+#include "LCGameInstance.h"
+
+ULCGameInstance::ULCGameInstance()
+{
+
+}
+
+ULCGameInstance::~ULCGameInstance()
+{
+
+}
---
+++ Project_Lethal/Source/Project_Lethal/Global/LCGameInstance.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "LCGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_LETHAL_API ULCGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+
+	ULCGameInstance();
+	~ULCGameInstance();
+};
