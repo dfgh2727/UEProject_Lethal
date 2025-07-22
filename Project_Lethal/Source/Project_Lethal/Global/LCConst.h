562c0b5b9b6bb9382105de8f1a07cde5599aa8e5
dfgh2727 <dfgh2727@naver.com>
2025-07-22 10:25:47 +0900

    이수환: LCConst 추가

---
+++ Project_Lethal/Source/Project_Lethal/Global/LCConst.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "LCConst.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_LETHAL_API ULCConst : public UObject
+{
+	GENERATED_BODY()
+	
+};
