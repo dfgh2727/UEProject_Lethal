89fd4e97cdbea4470c453a76af8297a359a8497e
dfgh2727 <dfgh2727@naver.com>
2025-07-22 15:04:44 +0900

    이수환: LCGlobalData 추가

---
+++ Project_Lethal/Source/Project_Lethal/Global/Data/LCGlobalData.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "LCGlobalData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_LETHAL_API ULCGlobalData : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
+
+// Data 헤더들을 추가해 Data에 접근하는 함수들을 만들 예정입니다.
