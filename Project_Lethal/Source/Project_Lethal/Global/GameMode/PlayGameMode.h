c5551780d63ac0c2199635aab508a921603eeb0e
dfgh2727 <dfgh2727@naver.com>
2025-07-28 01:40:43 +0900

    이수환: GameMode 추가

---
+++ Project_Lethal/Source/Project_Lethal/Global/GameMode/PlayGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Global/GameMode/LCGameMode.h"
+#include "PlayGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_LETHAL_API APlayGameMode : public ALCGameMode
+{
+	GENERATED_BODY()
+	
+};
---
+++ Project_Lethal/Source/Project_Lethal/Global/GameMode/ReadyGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Global/GameMode/LCGameMode.h"
+#include "ReadyGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_LETHAL_API AReadyGameMode : public ALCGameMode
+{
+	GENERATED_BODY()
+	
+};
