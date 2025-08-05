93a77308f595e7a35a0f48e37275aa2b87017071
siasiaaksen <jj818181@naver.com>
2025-08-05 16:42:47 +0900

    이재영 : PlayPlayerController 생성, 캐릭터 이동 1차

--- Project_Lethal/Source/Project_Lethal/Character/LCCharacter.h
+++ Project_Lethal/Source/Project_Lethal/Character/LCCharacter.h
@@ -19,11 +19,14 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, category = "LCCharacter", meta = (AllowPrivateAccess = "true"))
+	float Speed = 0.0f;
 
 };
